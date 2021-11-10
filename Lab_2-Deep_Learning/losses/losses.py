d64743b2108fdaceaa43be24e13ca2dc8b45b5c9
Arafa42 <s107661@ap.be>
2021-11-10 08:39:55 +0100

    project added + some changes in losses.py

---
+++ Lab_2-Deep_Learning/losses/losses.py
@@ -0,0 +1,10 @@
+import torch
+
+import utilities.utils as utils
+
+
+def mse(input_tensor: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    """TODO: implement this method"""
+    print(input_tensor)
+    print(target)
+    return []
