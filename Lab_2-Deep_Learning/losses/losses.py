93fa59ccf6aeddc7641d2547c4b3980077e2bf6c
Arafa42 <s107661@ap.be>
2021-11-10 08:43:26 +0100

    Mean squared finish

--- Lab_2-Deep_Learning/losses/losses.py
+++ Lab_2-Deep_Learning/losses/losses.py
@@ -1,10 +1,11 @@
 import torch
 
 import utilities.utils as utils
-
+import numpy as np
 
 def mse(input_tensor: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     """TODO: implement this method"""
     print(input_tensor)
     print(target)
-    return []
+    MSE = np.square(np.subtract(input_tensor,target)).mean()
+    return MSE
