e03aaf51d8f747e13399416332c9d95e30cc7259
Arafa42 <s107661@ap.be>
2021-11-10 10:22:48 +0100

    image fixed

--- Lab_2-Deep_Learning/losses/losses.py
+++ Lab_2-Deep_Learning/losses/losses.py
@@ -5,7 +5,5 @@ import numpy as np
 
 def mse(input_tensor: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     """TODO: implement this method"""
-    print(input_tensor)
-    print(target)
     MSE = np.square(np.subtract(input_tensor,target)).mean()
     return MSE
